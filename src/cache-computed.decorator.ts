0f1571fdeed84f6e45ddfa8e060acda48051845b
Berend <info@berendholtland.nl>
2022-11-15 18:57:15 +0100

    Added start for test for NestObjectBuilder

--- src/cache-computed.decorator.ts
+++ src/cache-computed.decorator.ts
@@ -1,22 +1,22 @@
-function CacheComputed() {
-    return function (
-        target: object,
-        propertyKey: string,
-        descriptor: PropertyDescriptor
-    ) {
-        const method = descriptor.get;
-        const name = `cachedComputed${propertyKey}`;
+export function CacheComputed() {
+	return function (
+		target: object,
+		propertyKey: string,
+		descriptor: PropertyDescriptor
+	) {
+		const method = descriptor.get;
+		const name = `cachedComputed${propertyKey}`;
 
-        descriptor.get = function () {
-            if (target.constructor.prototype[name]) {
-                return target.constructor.prototype[name];
-            }
+		descriptor.get = function () {
+			if (target.constructor.prototype[name]) {
+				return target.constructor.prototype[name];
+			}
 
-            if (method) {
-                const computed = method();
-                target.constructor.prototype[name] = computed;
-                return computed;
-            }
-        };
-    };
+			if (method) {
+				const computed = method();
+				target.constructor.prototype[name] = computed;
+				return computed;
+			}
+		};
+	};
 }
